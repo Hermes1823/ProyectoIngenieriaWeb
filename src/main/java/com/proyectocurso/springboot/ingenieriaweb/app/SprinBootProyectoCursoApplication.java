ae3b7e35848c507ba9daac792491143e4d188888
Hermes1823 <hermescallao47@gmail.com>
2024-06-18 22:58:20 -0500

    Iniciando proyecto de Ingenieria Web

---
+++ src/main/java/com/proyectocurso/springboot/ingenieriaweb/app/SprinBootProyectoCursoApplication.java
@@ -0,0 +1,13 @@
+package com.proyectocurso.springboot.ingenieriaweb.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootProyectoCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootProyectoCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectocurso/springboot/ingenieriaweb/app/SprinBootProyectoCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectocurso.springboot.ingenieriaweb.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootProyectoCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
